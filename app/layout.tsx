73157d80229d1268cfda244211e66ee1c6f115c7
AKameni1 <arthur.kamenitchualeu@gmail.com>
2025-01-15 00:54:16 -0500

    add speedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -45,6 +46,8 @@ export default function RootLayout({
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+
+        <SpeedInsights />
       </body>
     </html>
   )
